27f2c6afc648cdc2b7b61036bbfa1c272999ac15
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2016-06-17 21:57:15 +0200

    Added javadoc to PaginationContext - closes #7

--- src/main/java/hu/juzraai/toolbox/data/PaginationContext.java
+++ src/main/java/hu/juzraai/toolbox/data/PaginationContext.java
@@ -8,16 +8,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * Utility which helps you in calculations related to pagination links. The
+ * input is the record count and the number of records per page, {@link
+ * PaginationContext} can calculate record offset for a page index, the page
+ * count, and can generate pager links too.
+ *
  * @author Zsolt Jurányi
  * @since 16.07
  */
 public class PaginationContext {
-	// TODO doc
 
 	private final int recordCount;
 	private final int recordsPerPage;
 	private final int pageCount;
 
+	/**
+	 * Creates a new instance.
+	 *
+	 * @param recordCount    Total number of records
+	 * @param recordsPerPage Number of records displayed per page
+	 */
 	public PaginationContext(int recordCount, int recordsPerPage) {
 		Check.argument(0 <= recordCount, "recordCount must be non-negative");
 		Check.argument(0 < recordsPerPage, "recordsPerPage must be positive");
@@ -35,6 +45,14 @@ public class PaginationContext {
 		return recordsPerPage == that.recordsPerPage;
 	}
 
+	/**
+	 * For a given page index this method calculates the offset you can use e.g.
+	 * in an SQL query to fetch records of the given page. It's simply
+	 * calculated by the formula <code>pageIndex * recordsPerPage</code>.
+	 *
+	 * @param pageIndex Index of the page you need offset for
+	 * @return Offset to be used in query to fetch records for the page
+	 */
 	public int getOffset(int pageIndex) {
 		Check.argument(0 <= pageIndex, "pageIndex must be non-negative");
 		Check.argument(pageIndex < pageCount, "pageIndex must be less than pageCount");
@@ -45,6 +63,22 @@ public class PaginationContext {
 		return pageCount;
 	}
 
+	/**
+	 * Generates a list of page indexes to be shown. The list will contain at
+	 * most 3 sections: <ol> <li>The first page indexes from 0 till
+	 * <code>width-1</code></li> <li>A sequence of page indexes with a length of
+	 * <code>2*width-1</code>, where the element in the middle is
+	 * <code>currentPageIndex</code></li> <li>The last page indexes from
+	 * <code>pageCount-width</code> till <code>pageCount-1</code></li></ol>
+	 * <p>
+	 * It also generates a <code>null</code> element between sections, if the
+	 * two section doesn't have any common element. At these places you can
+	 * generate a "..." in your view.
+	 *
+	 * @param width            Width of each pagination link section
+	 * @param currentPageIndex Index of the current page
+	 * @return A list of page indexes to be displayed
+	 */
 	@Nonnull
 	public List<Integer> getPagesToShow(int width, int currentPageIndex) {
 		Check.argument(0 < width, "width must be positive");
@@ -64,10 +98,16 @@ public class PaginationContext {
 		return pages;
 	}
 
+	/**
+	 * @return Total number of records
+	 */
 	public int getRecordCount() {
 		return recordCount;
 	}
 
+	/**
+	 * @return Number of records displayed per page
+	 */
 	public int getRecordsPerPage() {
 		return recordsPerPage;
 	}
